050bfbef17326acaa9e1cd783fd8251895e9fde3
Matt Gambogi <m@gambogi.com>
2014-06-22 11:00:58 -0400

    Added most core evals routes

--- modules/evals.py
+++ modules/evals.py
@@ -7,3 +7,37 @@ bp = Blueprint('evals', __name__)
 @bp.route('/home')
 def home():
     return 'Welcome to evals!'
+
+@bp.route('/profile/<uid>')
+def profile( uid ):
+    return uid;
+
+@bp.route('/admin')
+def admin():
+    return 'this is for admin'
+
+@bp.route('/attendance')
+def attendance():
+    return 'this is for taking attendance'
+
+@bp.route('/meeting/<kind>/<ident>')
+def meeting( kind, ident ):
+    return 'A meeting of type ' + kind + ' with id ' + ident
+
+@bp.route('/meetings/<kind>')
+def meetings( kind ):
+    return 'All ' + kind + ' meetings'
+
+@bp.route('/projects')
+def projects:
+    return 'projects'
+
+@bp.route('/contributions')
+def contributions:
+    return '/contributions'
+
+@bp.route('/overview')
+@bp.route('/standings')
+@bp.route('/rankings')
+def overview():
+    return 'this is where you can see everyone'
